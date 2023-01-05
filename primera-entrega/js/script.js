1cf82d73fd6e9e7b48dcace4b0a8fae06fb3c307
Facu Frias <facu.fr87@gmail.com>
2023-01-05 11:14:46 -0300

    Se finalizó el script.js, resta html.

--- primera-entrega/js/script.js
+++ primera-entrega/js/script.js
@@ -2,40 +2,60 @@ console.log("¡Te damos la bienvenida a la Calculadora de propinas!");
 
 let porcentajePropina,
     cantidadPersonas,
-    totalPropina,
-    totalCuenta,
+    totalPropina = 0,
+    totalCuenta = 0,
     flag = false;
 
+while(flag === false) {
+  porcentajePropina = prompt("Ingresar porcentaje de propina.");
+  flag = validarNumero(porcentajePropina);
+}
 
+flag = false;
+while(flag === false) {
+  cantidadPersonas = prompt("Ingresar cantidad de comensales.");
+  flag = validarNumeroEntero(cantidadPersonas);
+}
 
-porcentajePropina = prompt("Ingresar porcentaje de propina.");
-
-
-cantidadPersonas = prompt("Ingresar cantidad de comensales.");
-
+flag = false;
 for(i=1; i<=cantidadPersonas; i++) {
   let montoPersona = prompt(`Ingresar monto de persona ${i}:`),
     flag = validarNumero(montoPersona);
   if(flag===false) {
     i--;
+  } else {
+    totalCuenta = Number(totalCuenta) + Number(montoPersona);
+    totalPropina = Number(totalPropina) + Number(calcularPropina(montoPersona, porcentajePropina));
+    alert(`Monto persona ${i}: ${montoPersona} - Porcentaje propina: ${porcentajePropina} - Propina: ${calcularPropina(montoPersona, porcentajePropina)}`);
   }
 }
+
+alert(`Monto total: ${totalCuenta}  -  Propina total: ${totalPropina}`);
+
 
 // Funciones
-function validarNumero(monto) {
-  if(isNaN(monto) || (Number(monto) <= 0)) {
-    alert("¡El monto ingresado es inválido!");
-    return false;
-  } else {
-    return true;
-  }
-}
 
-function validarNumeroEntero(valor) {
-  if(isNaN(valor) || (Number(valor) <= 0) || ) {
+// Determina que el valor ingresado sea numérico positivo
+function validarNumero(valor) {
+  if(isNaN(valor) || (Number(valor) <= 0)) {
     alert("¡El valor ingresado es inválido!");
     return false;
   } else {
     return true;
   }
+}
+
+// Determina que el valor ingresado sea numérico y entero positivo
+function validarNumeroEntero(valor) {
+  if(isNaN(valor) || (Number(valor) <= 0) || !Number.isInteger(Number(valor))) {
+    alert("¡El valor ingresado es inválido!");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Calcula el monto de propina que la persona debe poner
+function calcularPropina(monto, porcentaje) {
+  return (monto*(porcentaje/100));
 }
